20b4e8f95b1a792644bece71d25d6eac20815710
Jia-Yu <1easier10@gmail.com>
2023-09-15 15:06:27 +0800

    Feat: Add swiftLint.

---
+++ GoodThing/GoodThing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodThing
+//
+//  Created by 蔡佳峪 on 2023/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+
+
